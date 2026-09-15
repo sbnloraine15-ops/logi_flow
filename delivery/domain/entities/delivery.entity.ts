5e77af4e3a594aa306831a8c435e5f663ea20d9d
sabrina <sbnloraine@gmail.com>
2026-09-15 14:48:02 -0300

    atualização de status de delivery

--- delivery/domain/entities/delivery.entity.ts
+++ delivery/domain/entities/delivery.entity.ts
@@ -1,16 +1,22 @@
 import { Entity } from "../../../shared/domain/entites/entity.js"
 
+export enum DeliveryStatus {
+    PENDING = 'pending',
+    IN_PROGRESS = 'in_progress',
+    DELIVERED = 'delivered',
+    CANCELLED = 'cancelled'
+}
+
+
+
 export type DeliveryProps = {
-    pending: boolean
-    in_progress: boolean
-    delivered: boolean
-    cancelled: boolean
+    status: DeliveryStatus
     createdAt: Date
     driveId: string
 }
 
 export class DeliveryEntiry extends Entity<DeliveryProps> {
-    constructor(public readonly props: DeliveryProps, id?: string){
+    constructor(public readonly props: DeliveryProps, id?: string) {
         super(props, id)
         this.props.createdAt = this.props.createdAt ?? new Date()
     }
