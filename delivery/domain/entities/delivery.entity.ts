c7fa0cb809991cd366af1d1381ccef39d45c67d5
sabrina <sbnloraine@gmail.com>
2026-09-15 16:30:08 -0300

    implementando validators e repository

--- delivery/domain/entities/delivery.entity.ts
+++ delivery/domain/entities/delivery.entity.ts
@@ -12,15 +12,57 @@ export enum DeliveryStatus {
 export type DeliveryProps = {
     status: DeliveryStatus
     createdAt: Date
-    driveId: string
+    driverId: string
 }
 
-export class DeliveryEntiry extends Entity<DeliveryProps> {
-    constructor(public readonly props: DeliveryProps, id?: string) {
-        super(props, id)
-        this.props.createdAt = this.props.createdAt ?? new Date()
+export class DeliveryEntity extends Entity<DeliveryProps> {
+    constructor(
+        public readonly props: DeliveryProps, id?: string) {
+        super(
+            {
+                ...props,
+                status: props.status ?? DeliveryStatus.PENDING,
+                createdAt: props.createdAt ?? new Date(),
+            }, id
+        )
+
+    }
+
+     
+    get status(): DeliveryStatus {
+        return this.props.status
+    }
+
+    get driveId() {
+        return this.props.driverId
+    }
+
+    public assignDriver(driverId: string): void {
+        this.props.driverId = driverId
     }
 
 
+    public startDelivery(): void {
+        if (this.props.status !== DeliveryStatus.PENDING){
+            throw new Error('Apenas entregas pendentes podem ser iniciadas')
+        } 
+        this.props.status = DeliveryStatus.IN_PROGRESS
 
+    }
+
+    public completdDelivery(): void {
+        if(this.props.status !== DeliveryStatus.IN_PROGRESS){
+            throw new Error('Apenas entregas iniciadas podem ser concluidas')
+        }
+
+        this.props.status = DeliveryStatus.DELIVERED
+    }
+
+    public cancelDelivery(): void {
+        if(this.props.status !== DeliveryStatus.IN_PROGRESS){
+            throw new Error('Apenas entregas iniciadas podem ser canceladas')
+        }
+
+        this.props.status = DeliveryStatus.CANCELLED
+    }
 }
--- driver/domain/entities/driver.entity.ts
+++ driver/domain/entities/driver.entity.ts
@@ -1,7 +1,9 @@
+import { HttpException } from "@nestjs/common"
 import { Entity } from "../../../shared/domain/entites/entity.js"
+import { DriveValidatorFactory } from "../../validators/drive.validator.js"
 
 export type DriverProps = {
-    nome: string
+    name: string
     email: string
     vehicle: boolean
 }
@@ -11,6 +13,30 @@ export class DriverEntity extends Entity <DriverProps> {
         super(props, id)
     }
 
-    
+    upadate(value: string): void {
+        DriverEntity.validate({
+            ...this.props, 
+            name:value
+        })
+        this.name = value
+    }
+
+
+    get name(){
+        return this.props.name
+    }
+
+    private set name(value: string){
+        this.props.name = value
+    }
+    static validate(props: DriverProps) {
+        const validate = DriveValidatorFactory.create()
+        const isValid = validate.validate(props)
+        if(!isValid) {
+            throw new Error('Driver não é valido')
+        }
+    }
+
+
 }
 
